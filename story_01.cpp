c270685f0ad03e1a9bdb248c29197e90105b95bd
arpita mandloi <arpitamandloi369@gmail.com>
2025-05-12 13:59:56 +0530

    Updated story_01.cpp with improvements

--- story_01.cpp
+++ story_01.cpp
@@ -1,49 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void countFlowers(int N) {
+    int sunbuds = 0, moonblossoms = 0, starroots = 0;
+    int crystalFlowers = 0, wildleafs = 0, restingDays = 0;
+    int totalFlowers;
 
-void sunbuds(){
-    cout<<"Plant Two Sunbuds 🌻";
-}
+    for (int day = 1; day <= N; day++) {
+        if (day % 2 == 0 && day % 3 == 0 && day % 4 == 0) {
+            restingDays++; 
+        } 
+         else if (day % 2 == 0 && day % 3 == 0) {
+            crystalFlowers++; 
+        } 
 
-
-
-
-
-
-int planting_flowers(int day){
-     
-    for(int i = 0 ; i < day ; i++){
-        if(day % 2==0){
-         sunbuds();
-         return 2;
-        }
-        else if(day % 3 == 0){
-            return 3;
-        }
-        else if(day % 2 == 0 && day % 3 == 0){
-            return 1;
-        }
-        else if (day % 2 == 0 && day % 3 == 0 && day % 4 == 0){
-            return 0;
-        }
-        else{
-            cout<<"plant 1 wildlife";
-            return -1;
+        else if (day % 2 == 0) {
+            sunbuds += 2;
+        } 
+        else if (day % 3 == 0) {
+            moonblossoms += 3;
+        } 
+        else if (day % 4 == 0) {
+            starroots += 4;
+       } 
+       else {
+            wildleafs++; 
         }
     }
+
     
+    cout << "🌻 Sunbuds planted: " << sunbuds << endl;
+    cout << "🌙 Moonblossoms planted: " << moonblossoms << endl;
+    cout << "⭐ Starroots planted: " << starroots << endl;
+    cout << "💎 Crystal Flowers planted: " << crystalFlowers << endl;
+    cout << "🍃 Wildleafs planted: " << wildleafs << endl;
+    cout << "⏳ Days soil was resting: " << restingDays << endl;
 
+  
+     totalFlowers = sunbuds + moonblossoms + starroots + crystalFlowers + wildleafs;
+    cout << "🌼 Total flowers planted: " << totalFlowers << endl;
 }
 
-
-
-int main(){
-    int day;
-    cout<<"Enter a day to palnt flowers:"<<endl;
-    cin>>day;
-    int result = planting_flowers(day);
-    if (result != -1){
-        cout<<"Planting type:"<<result<<endl;
-    }
+int main() {
+    int N;
+    cout << "Enter the number of days: ";
+    cin >> N;
+    countFlowers(N);
+    return 0;
 }
